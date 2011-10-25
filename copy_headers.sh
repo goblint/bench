08bbf1c56532d3062e2642a7cde41ff05dde3d45
Vesal Vojdani <vesal.vojdani@gmail.com>
2011-10-25 16:07:56 +0200

    update bench

--- copy_headers.sh
+++ copy_headers.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Prepare Kernel: make defconfig && make prepare
 # Then run this script.
-rm -rf linux-headers
+rm -rf linux-headers linux-headers-orig
 mkdir linux-headers
 cd linux-headers
 cp -Lr $1/include .
 mkdir -p arch/x86
 cp -Lr $1/arch/x86/include arch/x86/
 cp $1/.config ./config.txt
+cd ..
+cp -Lr linux-headers linux-headers-orig
 #Create Patch: diff -ru linux-headers-orig linux-headers > headers.patch
 #Apply patch: patch -p1 -d linux-headers <headers.patch
