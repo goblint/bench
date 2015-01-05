b1b46392db93dfcd0f6a00f4dfa76a960f9c7f6c
Vesal Vojdani <vesal.vojdani@gmail.com>
2015-01-05 19:53:03 +0200

    remove headers from repo

--- linux-headers/arch/x86/include/generated/asm/.syscalls_32.h.cmd
+++
@@ -1,1 +0,0 @@
-cmd_arch/x86/syscalls/../include/generated/asm/syscalls_32.h := /bin/bash './arch/x86/syscalls/syscalltbl.sh' arch/x86/syscalls/syscall_32.tbl arch/x86/syscalls/../include/generated/asm/syscalls_32.h
