3c8751f749f1b33a7a69d14e5921114470f348ee
Ralf Vogler <ralf.vogler@gmail.com>
2020-05-29 15:56:17 +0200

    update parallel.sh

--- parallel.sh
+++ parallel.sh
@@ -13,21 +13,23 @@ function finish {
 }
 trap finish EXIT
 
-opt="--enable keepcpp --enable dbg.verbose --enable dbg.debug --set ana.activated \"['base']\" --enable exp.earlyglobs --enable exp.full-context --enable ana.int.interval --enable ana.int.enums --disable ana.int.trier --enable printstats"
+opt="--enable dbg.verbose --enable printstats --set ana.activated \"['base']\" --enable exp.earlyglobs --enable ana.int.interval --disable ana.int.enums --disable ana.int.def_exc --enable exp.full-context --disable exp.widen-context --disable noverify --disable exp.solver.td3.space_restore --disable exp.solver.td3.space --enable exp.solver.td3.term --enable exp.solver.td3.space_cache --enable dbg.trace.context"
 # --enable exp.no-interval32-context # needed for wget b/c of mergesort
 # --enable exp.no-int-context # needed for '400.perlbench_comb.c 482.sphinx_livepretend_comb.c duff-0.5_comb.c maradns-1.4.06_comb.c'. Only '445.gobmk_comb.c' does not terminate.
 # --enable ana.hashcons # no(t much) difference in runtime or max res. size for wget. TODO try rest
-cmd="(date && $time -v ./goblint $opt --sets solver slr3t --sets comparesolver slr3tp {1} 2>&1) | tee out/{1}.cmp.log"
+# cmd="(date && $time -v ./goblint $opt --sets solver topdown --sets comparesolver topdown_term {1} 2>&1) > out/{1}.cmp.log"
 # parallel --noswap --memfree 2G --load 100% --eta --joblog out/jobs.log --tmux --dryrun $cmd ::: single-thread/*.c
-parallel --noswap --load 100% --eta --joblog out/jobs.log $cmd ::: $files
-ag locals out/$dir/*.cmp.log | sort | tee out/$dir.locals
+# parallel --noswap --load 100% --eta --joblog out/jobs.log $cmd ::: $files
+# ag locals out/$dir/*.cmp.log | sort | tee out/$dir.locals
 
-cmd="(date && $time -v ./goblint $opt --sets solver {1} {2} 2>&1) | tee out/{2}.{1}-solver.log"
-parallel --noswap --load 100% --eta --joblog out/jobs.log $cmd ::: slr3t slr3tp ::: $files
-ag "vars =" out/$dir/*-solver.log | sort | tee out/$dir.vars
+cmd="(date && $time -v ./goblint {2} $opt --sets solver td3 --{1} exp.solver.td3.space 2>&1) > out/{2}.{1}-space.log"
+parallel --noswap --load 100% --eta --joblog out/jobs.log $cmd ::: enable disable ::: $files
+ag "vars =" out/$dir/*-space.log | sort | tee out/$dir.vars
 
-ag "exp.no-int-context" out/$dir -l | sort | tee out/$dir.no-int-context
-ag "exp.no-interval32-context" out/$dir -l | sort | tee out/$dir.no-interval32-context
+# ag "exp.no-int-context" out/$dir -l | sort | tee out/$dir.no-int-context
+# ag "exp.no-interval32-context" out/$dir -l | sort | tee out/$dir.no-interval32-context
 ag -i fixpoint out/$dir | tee out/$dir.fixpoint
-ag exception out/$dir | tee out/$dir.exception
+ag 'Fatal error:' out/$dir | tee out/$dir.error
 ag "terminated by signal" out/$dir | tee out/$dir.terminated
+# ag "wall clock" out/coreutils | sed 's/:.*\t/\t/' | sed 's/Elapsed (wall clock) time (h:mm:ss or m:ss): //' | sort -n -k 2 | column -t
+# ag "wall clock" out/single-thread/*.wpoint_simple* | sed 's/:.*\t/\t/' | sed 's/Elapsed (wall clock) time (h:mm:ss or m:ss): //' | sort -n -k 2 | xargs -n2 printf "%-75s\t%12s\n"
