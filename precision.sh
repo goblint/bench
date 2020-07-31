07000c4b1df0cd34f94193f1e4fc75a325fcf76c
Ralf Vogler <ralf.vogler@gmail.com>
2020-07-31 02:26:37 +0200

    precision.sh: compare_runs

---
+++ precision.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# compare the precision of [variants] in [dir]
+dir=${1-"out"}
+variants=${2-'.'} # e.g. enable|disable
+runs=$(find $dir -type d -name '*.c*' | egrep $variants | sort)
+programs=$(echo "$runs" | sed 's/\.c.*/\.c/' | sort -u)
+for program in $programs; do
+  program_runs=$(echo "$runs" | grep $program)
+  n=$(echo "$program_runs" | wc -l)
+  if [ $n -ne 2 ]; then
+    echo "Skipping since it has $n variant(s) instead of 2: $program";
+    continue;
+  fi
+  src=$(echo $program | sed "s/^$dir\///")
+  if [ ! -f $src ]; then
+    echo "Source $src for $program does not exist!";
+    continue;
+  fi
+  dst=$program.precision
+  rm -f $dst
+  # TODO change this to ./goblint once file is no longer busy
+  ~/analyzer/goblint --compare_runs $program_runs $src 2>/dev/null | sed '/^$/d' | tee $dst
+done
