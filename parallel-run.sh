f9dd3cf129b510e149c5ac9de42effd5b659b00e
Ralf Vogler <ralf.vogler@gmail.com>
2021-03-18 17:35:43 +0100

    use default ana.activated since threadflag etc. was refactored out from base

--- parallel-run.sh
+++ parallel-run.sh
@@ -21,7 +21,8 @@ function finish {
 trap finish EXIT
 
 # defaults: --enable verify --enable exp.solver.td3.space_cache --disable dbg.trace.context --sets solver td3
-opts="--set ana.activated \"['base','mallocWrapper']\" --enable exp.earlyglobs --enable ana.int.interval --disable ana.int.enums --disable ana.int.def_exc --enable exp.full-context --disable exp.widen-context\
+# --set ana.activated \"['base','mallocWrapper']\"; defaults: "['expRelation','base','threadid','threadflag','escape','mutex', 'mallocWrapper']"
+opts="--enable exp.earlyglobs --enable ana.int.interval --disable ana.int.enums --disable ana.int.def_exc --enable exp.full-context --disable exp.widen-context\
   --disable exp.solver.td3.space_restore"
 # --enable exp.no-interval32-context # needed for wget b/c of mergesort
 # --enable exp.no-int-context # needed for '400.perlbench_comb.c 482.sphinx_livepretend_comb.c duff-0.5_comb.c maradns-1.4.06_comb.c'. Only '445.gobmk_comb.c' does not terminate.
