94c7c473ee4d41288f9cc1261bb3bb35654816a4
Simmo Saan <simmo.saan@gmail.com>
2021-06-15 13:01:05 +0000

    Update CPALockator benchmarks with groups, privs and exlusion of failed

--- sosy-ldv-index.sh
+++ sosy-ldv-index.sh
@@ -2,12 +2,19 @@
 
 # Usage: ./sosy-ldv-index.sh >> index/traces-cpalockator.txt
 
-for file in sosy-ldv/linux-4.2.6-races/*/*.i; do
-    name="$(basename "$file" .ko.cil.i)"
-    name="${name/?__linux-concurrency_safety__drivers---net---/}"
-    echo $name
-    echo $name
-    echo $file
-    echo "--enable ana.sv-comp.functions"
+for subdir in Safes Unknowns Unsafes; do
+    echo "Group: CPALockator linux-4.2.6-races ($subdir)"
     echo
+    for file in sosy-ldv/linux-4.2.6-races/$subdir/*.i; do
+        name="$(basename "$file" .ko.cil.i)"
+        name="${name/?__linux-concurrency_safety__drivers---net---/}"
+
+        if ! grep -q "^$name$" ./sosy-ldv-failed.txt; then
+            echo $name
+            echo $name
+            echo $file
+            echo "--enable ana.sv-comp.functions"
+            echo
+        fi
+    done
 done
