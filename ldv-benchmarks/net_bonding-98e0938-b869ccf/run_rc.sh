df6a2a73cc09e3088442a371c1b8a03096ac3e1f
Kalmer Apinis <kalmer.apinis@gmail.com>
2022-03-08 15:57:43 +0200

    6 benches from ldv-benchmark (sources)

---
+++ ldv-benchmarks/net_bonding-98e0938-b869ccf/run_rc.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+../analyzer/goblint --enable kernel --set pre.kernel-root . bond_sysfs_rc.c bond_3ad.c  bond_alb.c  bond_debugfs.c  bond_main.c  bond_netlink.c  bond_options.c  bond_procfs.c
