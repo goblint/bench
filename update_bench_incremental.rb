f82578960f44b1b5cc2a726c2cf84196d50b3f00
Simmo Saan <simmo.saan@gmail.com>
2022-03-29 12:35:40 +0300

    Add deadlock def and reporting

--- update_bench_incremental.rb
+++ update_bench_incremental.rb
@@ -89,10 +89,12 @@ def print_file_res (f, path)
               unsafe = lines.grep(/unsafe:[ ]*([0-9]*)/) { |x| $1.to_i } .first
               total = lines.grep(/total:[ ]*([0-9]*)/) { |x| $1.to_i } .first
               uncalled = lines.grep(/will never be called/).reject {|x| x =~ /__check/}.size
+              deadlock = lines.grep(/\[Deadlock\]/).size
               thenumbers =  "<font color=\"green\">#{safely}</font>+"
               thenumbers << "<font color=\"orange\">#{vulner}</font>+"
               thenumbers << "<font color=\"red\">#{unsafe}</font>="
               thenumbers << "#{total}"
+              thenumbers << "; <font color=\"blue\">#{deadlock}</font>"
               thenumbers << "; <font color=\"magenta\">#{uncalled}</font>" if uncalled > 0
             end
             thenumbers = " (#{thenumbers})" unless thenumbers.nil?
