37bf6bc2baf585892d8cf7e035757d765beab61b
Simmo Saan <simmo.saan@gmail.com>
2022-04-12 10:51:53 +0300

    Fix type cast in ddverify elevator stub

--- ddverify/models/con1/src/linux/block/elevator.c
+++ ddverify/models/con1/src/linux/block/elevator.c
@@ -6,7 +6,7 @@ struct request *elv_next_request(request_queue_t *q)
   int genhd_no = q->__ddv_genhd_no;
 
   if (genhd_registered[genhd_no].requests_open > 0) {
-    return genhd_registered[genhd_no].current_request;
+    return &genhd_registered[genhd_no].current_request;
   } else {
     return NULL;
   }
