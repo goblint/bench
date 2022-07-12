e7e9be5a1fcb7f6da28563d75b372cdf0e4398cf
Julian Erhard <julian.erhard@tum.de>
2022-07-12 10:18:43 +0200

    Ratcop-benchmarks: Outcomment asserts annotated with UNKNOWN../bench/update_bench_traces_rel_ratcop.rb
    UKNOWN! asserts should only be contained in the regression tests.

--- ratcop/17-mukherjee_sync01.c
+++ ratcop/17-mukherjee_sync01.c
@@ -29,9 +29,9 @@ int main() {
     pthread_join(t1, 0);
     pthread_join(t2, 0);
 
-    assert(num >= 0); // UNKNOWN!
+    // assert(num >= 0); // UNKNOWN!
 
-    assert(num <= 1); // UNKNOWN!
+    // assert(num <= 1); // UNKNOWN!
 
     return 0;
 }
