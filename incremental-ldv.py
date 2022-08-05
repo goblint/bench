3fd8d32483fec11dc39542a13eaa0bb523e55468
Julian Erhard <julian.erhard@tum.de>
2022-08-05 15:07:32 +0200

    Add some class structure to benchmark results

--- incremental-ldv.py
+++ incremental-ldv.py
@@ -1,10 +1,51 @@
 from cProfile import run
 import os
 import shutil
+import time
 from pathlib import Path
 
 path = "../bench-repos/regression-verification-tasks/regression-verification-tasks/"
-d = "drivers--auxdisplay--cfag12864b.ko"
+
+class Result:
+    file = ""
+    runtime = 0
+    configuration = None
+    output = ""
+
+    def __init__(self, file, runtime, configuration, output):
+        self.file = file
+        self.runtime = runtime
+        self.configuration = configuration
+        self.output = output
+
+# results for a sequence of commits
+class Sequence:
+    results = []
+
+    def __init__(self, results):
+        self.results = results
+
+class FolderResult:
+    folder = ""
+    sequences = []
+
+    def __init__(self, folder, sequences):
+        self.folder = folder
+        self.sequences = sequences
+
+class ConfigMappings:
+    mappings = {} # mapping from configs to FolderResults
+
+    def __init__(self, mappings):
+        self.mappings = mappings
+
+class Configuration:
+    config = ""
+    name = ""
+
+    def __init__(self, config, name):
+        self.config = config
+        self.name = name
 
 def run_folder(path):
     files = os.listdir(path)
@@ -59,22 +100,48 @@ def run_goblint(file, configuration):
     if Path(destination).is_file():
         os.remove(destination)
     shutil.copyfile(file, destination)
-    cmd = "./goblint " + destination + " " + configuration
+    cmd = "./goblint " + destination + " " + configuration.config
+    before = time.time_ns()
     stream = os.popen(cmd)
+    diff = time.time_ns() - before
     output = stream.read()
-    output
+    Result(file, diff, configuration, output)
 
-def benchmark(dir, benchmarks):
+def benchmark_folder(dir, configuration: Configuration):
+    dir = dir
+    benchmarks = find_sequences(dir)
+
+    if benchmarks == None:
+        return
+
+    sequences = []
     for key in benchmarks:
+        seq = []
         for file in benchmarks.get(key):
             file = os.path.join(dir, file)
-            run_goblint(file, "")
+            result = run_goblint(file, configuration)
+        seq.append(result)
+        sequences.append(Sequence(seq))
+    return FolderResult(dir, sequences)
+
+def benchmark_all_folders(path, configuration: Configuration):
+    folder_results = []
+    for dir in os.listdir(path):
+        path = os.path.join(path, dir)
+        folder_result = benchmark_folder(path, configuration)
+        folder_results.append(folder_result)
+    return folder_result
+
+def configurations():
+    incremental = Configuration("--enable incremental.save --enable incremental.load", "incremental")
+    non_incremental = Configuration("", "non-incremental")
+    return [incremental, non_incremental]
 
 def run_benchmarks(path):
-    for dir in os.listdir(path):
-        dir = path + dir
-        seqs = find_sequences(dir)
-        if seqs != None:
-            benchmark(dir, seqs)
+    config_mapping = {}
+    for conf in configurations():
+        results = benchmark_all_folders(path, conf)
+        config_mapping[conf.name] = results
+    return ConfigMappings(config_mapping)
 
 run_benchmarks(path)
