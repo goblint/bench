e1388676014a19581ed710b276ff738bccec5337
Julian Erhard <julian.erhard@tum.de>
2022-10-06 12:41:31 +0200

    Add build_compdb_sqlite.sh

---
+++ scripts/incremental/build/build_compdb_sqlite.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# sed -i 's/zstd : CPPFLAGS += -DZSTD_LEGACY_SUPPORT=$(ZSTD_LEGACY_SUPPORT).*/zstd : CPPFLAGS += -DZSTD_LEGACY_SUPPORT=$(ZSTD_LEGACY_SUPPORT) -DZSTD_NO_INTRINSICS/' programs/Makefile
+sh configure
+LC_ALL=C.UTF-8 compiledb make -j 1 sqlite3.lo
