0345ce7123ce5aa890b60280721ff99dc275343b
Julian Erhard <julian.erhard@tum.de>
2022-11-29 22:26:02 +0100

    Update build script for chrony.

--- scripts/incremental/build/build_compdb_chrony.sh
+++ scripts/incremental/build/build_compdb_chrony.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# git clean -fdx
-# ./configure
-# make -j 1 chronyd | tee build.log
-# compiledb --parse build.log
-./configure && bear -- make chronyd
+git clean -fdx
+./configure
+make -j 1 chronyd | tee build.log
+compiledb --parse build.log
+# ./configure && bear -- make chronyd
