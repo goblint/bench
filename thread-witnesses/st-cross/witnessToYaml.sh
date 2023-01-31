662e05b2f5bfdba4d6a0a971895007a4761b92c0
Simmo Saan <simmo.saan@gmail.com>
2023-01-31 08:21:52 +0000

    Update paths in witnessToYaml

--- thread-witnesses/st-cross/witnessToYaml.sh
+++ thread-witnesses/st-cross/witnessToYaml.sh
@@ -2,19 +2,21 @@
 
 MYBENCHDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 AUTOACTIVEDIR=$MYBENCHDIR/../../../autoactive
+RESULTSGENYAML=resultsgenyaml
+GENERATEDYAML=generated_yaml
 
 cp $MYBENCHDIR/witnessToYaml.xml $AUTOACTIVEDIR/plain/
 
 cd $AUTOACTIVEDIR
 
-# rm -rf resultsgenyaml
-benchexec --tool-directory ../CPAchecker-2.2-unix -N5 -c1 --read-only-dir / --hidden-dir /home --overlay-dir . plain/witnessToYaml.xml -o resultsgenyaml2/
+# rm -rf $RESULTSGENYAML
+benchexec --tool-directory ../CPAchecker-2.2-unix -N5 -c1 --read-only-dir / --hidden-dir /home --overlay-dir . plain/witnessToYaml.xml -o $RESULTSGENYAML/
 
-# rm -rf generated_yaml
+# rm -rf $GENERATEDYAML
 for tool in $(cat producers.txt) ; do
-  mkdir -p generated_yaml2/$tool ;
-  for dir in resultsgenyaml2/*files/$tool/* ; do
+  mkdir -p $GENERATEDYAML/$tool ;
+  for dir in $RESULTSGENYAML/*files/$tool/* ; do
     echo $dir
-    cat $dir/output/invariantWitnesses/*.yaml | sed -e 's|plain|../plain|g' -e 's|loop_invariant|location_invariant|g' > generated_yaml2/$tool/$(basename $dir).witness.yml
+    cat $dir/output/invariantWitnesses/*.yaml | sed -e 's|plain|../plain|g' -e 's|loop_invariant|location_invariant|g' > $GENERATEDYAML/$tool/$(basename $dir).witness.yml
   done
 done
