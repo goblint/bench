e5b1285ee55cd17a0364f942dc54f9016b0b086a
Jonas August <ge59duv@mytum.de>
2023-07-11 17:56:17 +0200

    Add issue to longjump exception

--- incremental-test-generation/scripts/generate_programs.py
+++ incremental-test-generation/scripts/generate_programs.py
@@ -116,10 +116,10 @@ def _fix_params(params):
     params = re.sub(r'--set [\'"]*ana\.activated\[\+\][\'"]* [\'"]*file[\'"]*', '', params)
 
     # Optimisations activate the compiler flag -fmove-loop-stores which causes problems with the incremental analysis
-    params = re.sub(r'--set [\'"]*pre\.cppflags\[\+\][\'"]* [\'"]*-O1[\'"]*', '', params)
-    params = re.sub(r'--set [\'"]*pre\.cppflags\[\+\][\'"]* [\'"]*-O2[\'"]*', '', params)
-    params = re.sub(r'--set [\'"]*pre\.cppflags\[\+\][\'"]* [\'"]*-O3[\'"]*', '', params)
-    params = re.sub(r'--set [\'"]*pre\.cppflags\[\+\][\'"]* [\'"]*-fmove-loop-stores[\'"]*', '', params)
+    #params = re.sub(r'--set [\'"]*pre\.cppflags\[\+\][\'"]* [\'"]*-O1[\'"]*', '', params)
+    #params = re.sub(r'--set [\'"]*pre\.cppflags\[\+\][\'"]* [\'"]*-O2[\'"]*', '', params)
+    #params = re.sub(r'--set [\'"]*pre\.cppflags\[\+\][\'"]* [\'"]*-O3[\'"]*', '', params)
+    #params = re.sub(r'--set [\'"]*pre\.cppflags\[\+\][\'"]* [\'"]*-fmove-loop-stores[\'"]*', '', params)
     
     if params_inital != params:
         print(f'{COLOR_YELLOW}[WARNING] Some parameters from the PARAM string in the input file (grey) were removed to avoid crashing the tester:{COLOR_RESET} {params} {COLOR_GREY}{params_inital}{COLOR_RESET}')
