d86f8ac6ec50c390933f95078753d8dd867ec79e
Jonas August <ge59duv@mytum.de>
2023-07-12 14:46:30 +0200

    Remove git implementation

--- incremental-test-generation/scripts/generate_programs.py
+++ incremental-test-generation/scripts/generate_programs.py
@@ -1,13 +1,12 @@
 from add_check import add_check
 from add_check_annotations import add_check_annotations
-from generate_git import *
 from generate_ml import *
 from generate_mutations import *
 
 
 # generates programs in the temp_dir
 def generate_programs(source_path, temp_dir, clang_tidy_path, goblint_path, apikey_path, mutations, enable_mutations,
-                      enable_ml, enable_git, enable_precision, ml_count, ml_select, ml_interesting, ml_16k, git_start, git_end, generate_git_build_path, include_paths):
+                      enable_ml, enable_precision, ml_count, ml_select, ml_interesting, ml_16k, include_paths):
     # Clean working directory
     if os.path.isdir(temp_dir):
         shutil.rmtree(temp_dir)
@@ -17,7 +16,7 @@ def generate_programs(source_path, temp_dir, clang_tidy_path, goblint_path, apik
     with open(meta_path, 'w') as outfile:
         yaml.dump({'n': 0, 'p_0': {META_TYPE: GenerateType.INITAL.value}}, outfile)
     # Copy the source program into the temp dir
-    program_path = os.path.join(temp_dir, 'p.c' if not enable_git else 'p.sh')
+    program_path = os.path.join(temp_dir, 'p.c')
     shutil.copy2(source_path, program_path)
     program_0_path = os.path.join(temp_dir, 'p_0.c')
     shutil.copy2(source_path, program_0_path)
@@ -37,31 +36,26 @@ def generate_programs(source_path, temp_dir, clang_tidy_path, goblint_path, apik
     if enable_ml:
         index = generate_ml(program_0_path, apikey_path, meta_path, ml_count, ml_select, ml_interesting, ml_16k)
 
-    if enable_git:
-        index = generate_git(goblint_path, temp_dir, meta_path, program_0_path, git_start, git_end, generate_git_build_path)
-
     # Add checks with annotations
     print_separator()
-    if enable_git:
-        print('Generating goblint checks. This may take a while...')
     params = _get_params_from_file(program_0_path)
     params = _fix_params(params)
     for i in range(index + 1):
         print(f"\r[{i}/{index}] Generating goblint checks...", end='')
         sys.stdout.flush()
         file_path = os.path.join(temp_dir, f"p_{i}.c")
-        compiling = add_check(file_path, goblint_path, meta_path, params, i, enable_git)
+        compiling = add_check(file_path, goblint_path, meta_path, params, i)
         if not compiling:
             continue
         file_path = os.path.join(temp_dir, f"p_{i}_check.c")
         # For the patched file generate NOFAIL / NOTINPRECISE annotations
-        if i == 0 or enable_git:
+        if i == 0:
             if enable_precision:
                 add_check_annotations(file_path, 'NOTINPRECISE')
             else:
                 add_check_annotations(file_path, 'NOFAIL')
         # For the inital file generate SUCCESS annotations
-        if i != 0 or enable_git:
+        if i != 0:
             add_check_annotations(file_path, 'SUCCESS')
     print(f"\r{COLOR_GREEN}Generating goblint checks [DONE]{SPACE}{COLOR_RESET}")
 
@@ -159,21 +153,18 @@ def _preserve_goblint_check_annotations(file_path):
 
 def main():
     parser = argparse.ArgumentParser(description='Generate programs in the working directory')
-    parser.add_argument('source_path', help='Path to the source program or git sh file provided by the user')
+    parser.add_argument('source_path', help='Path to the source program provided by the user')
     parser.add_argument('temp_dir', help='Path to the working directory')
     parser.add_argument('clang_tidy_path', help='Path to the modified clang-tidy executable')
     parser.add_argument('goblint_path', help='Path to the goblint executable')
     parser.add_argument('--apikey-path', help='Path to the API')
     parser.add_argument('--enable-mutations', action='store_true', help='Enable Mutations. When no mutation is selected all are activated.')
     parser.add_argument('--enable-ml', action='store_true', help='Enable ML')
-    parser.add_argument('--enable-git', action='store_true', help='Enable Git')
     parser.add_argument('--enable-precision', action='store_true', help='Enable generation for precision tests')
     parser.add_argument('--ml-count', type=int, default=DEFAULT_ML_COUNT, help='Number of ML programs to generate')
     parser.add_argument('--ml-select', type=int, default=DEFAULT_ML_SELECT, help='Number of selected lines for ML')
     parser.add_argument('--ml-interesting', default="[]", help='Lines to randomly choose the start line for selection (Default are all lines)')
     parser.add_argument('--ml-16k', action='store_true', help='Use the 16k mode for ml')
-    parser.add_argument('--git-start', help='The starting commit hash for git generation')
-    parser.add_argument('--git-end', help='The ending commit hash for git generation')
 
     # Add mutation options
     add_mutation_options(parser)
@@ -181,12 +172,8 @@ def main():
     args = parser.parse_args()
 
     # At least one generator has to be enabled
-    if not args.enable_mutations and not args.enable_ml and not args.enable_git:
-        parser.error("At least one generator has to be enabled (--enable_mutations, --enable-ml, --enable-git)")
-
-    # If using git, only git can be used
-    if args.enable_git and (args.enable_ml or args.enable_mutations):
-        parser.error("--enable-git cannot be used with --enable-ml or --enable-mutations")
+    if not args.enable_mutations and not args.enable_ml:
+        parser.error("At least one generator has to be enabled (--enable_mutations, --enable-ml)")
 
     # If all mutation options are false, set all to true
     mutations = get_mutations_from_args(args)
@@ -202,15 +189,9 @@ def main():
     if args.ml_interesting != "[]" and validate_interesting_lines(args.ml_interesting, None) is None:
         sys.exit(RETURN_ERROR)
 
-    # Check git commit hashes
-    git_start_commit = args.git_start
-    git_end_commit = args.git_end
-    if (git_start_commit is None and git_end_commit is not None) or (git_start_commit is not None and git_end_commit is None):
-        parser.error('[ERROR] Give a git start commit hash AND a end commit hash')
-
     generate_programs(args.source_path, args.temp_dir, args.clang_tidy_path, args.goblint_path, args.apikey_path,
-                      mutations, args.enable_mutations, args.enable_ml, args.enable_git, args.enable_precision, args.ml_count, args.ml_select,
-                      args.ml_interesting, args.ml_16k, args.git_start, args.git_end, './generate_git_build.sh', [])
+                      mutations, args.enable_mutations, args.enable_ml, args.enable_precision, args.ml_count, args.ml_select,
+                      args.ml_interesting, args.ml_16k, [])
 
 
 if __name__ == '__main__':
