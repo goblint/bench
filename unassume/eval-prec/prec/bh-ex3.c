cba2c9e654c0f11f9b8c133f95c1ac37e797b9ee
Simmo Saan <simmo.saan@gmail.com>
2023-07-31 12:18:37 +0300

    Copy st-lit to unassume/eval-prec

---
+++ unassume/eval-prec/prec/bh-ex3.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+extern void abort(void);
+void reach_error() { assert(0); }
+void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: {reach_error();abort();} } }
+extern _Bool __VERIFIER_nondet_bool();
+
+int main() {
+  int m = 0;
+  int n = 0;
+  while (1) {
+    __VERIFIER_assert(m <= 60);
+    __VERIFIER_assert(n <= 60);
+    if (__VERIFIER_nondet_bool()) {
+      if (__VERIFIER_nondet_bool()) {
+        if (m < 60) {
+          m++;
+        }
+        else {
+          __VERIFIER_assert(m == 60);
+          m = 0;
+        }
+      }
+    }
+    else {
+      if (__VERIFIER_nondet_bool()) {
+        if (n < 60) {
+          n++;
+        }
+        else {
+          __VERIFIER_assert(n == 60);
+          n = 0;
+        }
+      }
+    }
+  }
+  return 0;
+}
---
+++ unassume/eval-prec/prec/mine-tutorial-ex4.10.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+extern void abort(void);
+void reach_error() { assert(0); }
+void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: {reach_error();abort();} } }
+
+int main() {
+  int v = 1; // Not explicitly stated in Miné's example
+  while (v <= 50) {
+    __VERIFIER_assert(1 <= v);
+    v += 2;
+    __VERIFIER_assert(v <= 52);
+  }
+  __VERIFIER_assert(51 <= v);
+  __VERIFIER_assert(v <= 52);
+  return 0;
+}
