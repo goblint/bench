ed57b6bf52c0d6d89983016190414ce60f433a90
Simmo Saan <simmo.saan@gmail.com>
2023-10-06 12:31:24 +0000

    Run SV-COMP 2023 tools on Concrat

--- concrat/run.sh
+++ concrat/run.sh
@@ -5,12 +5,22 @@ set -e
 
 MYBENCHDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # RESULTSDIR=$MYBENCHDIR/../results/eval-prec
-RESULTSDIR=/mnt/goblint-svcomp/benchexec/results/pldi-02-goblint-concrat-svcomp-conf
+RESULTSDIR=/mnt/goblint-svcomp/benchexec/results/pldi-03-svcomp23-tools
 # GOBLINTDIR=$MYBENCHDIR/../goblint
-GOBLINTDIR=/mnt/goblint-svcomp/sv-comp/goblint
-GOBLINT_PARALLEL=7
-# CPACHECKER_PARALLEL=2
-# ULTIMATE_PARALLEL=2
+# GOBLINTDIR=/mnt/goblint-svcomp/sv-comp/goblint
+# GOBLINT_PARALLEL=7
+CPACHECKER_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/CPAchecker-2.2-unix
+CPACHECKER_PARALLEL=7
+DARTAGNAN_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/dartagnan
+DARTAGNAN_PARALLEL=7
+DEAGLE_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/deagle
+DEAGLE_PARALLEL=7
+LOCKSMITH_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/locksmith
+LOCKSMITH_PARALLEL=7
+THETA_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/theta
+THETA_PARALLEL=7
+ULTIMATE_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/UAutomizer-linux
+ULTIMATE_PARALLEL=7
 
 # read-only and overlay dirs for Value too large for defined data type workaround
 BENCHEXEC="benchexec --read-only-dir / --overlay-dir . --overlay-dir /home --outputpath $RESULTSDIR"
@@ -18,26 +28,37 @@ BENCHEXEC="benchexec --read-only-dir / --overlay-dir . --overlay-dir /home --out
 mkdir $RESULTSDIR || true
 
 # Run verification
-cd $GOBLINTDIR
-$BENCHEXEC --numOfThreads $GOBLINT_PARALLEL $MYBENCHDIR/goblint.xml
+# cd $GOBLINTDIR
+# $BENCHEXEC --numOfThreads $GOBLINT_PARALLEL $MYBENCHDIR/goblint.xml
 
-# cd $MYBENCHDIR/../CPAchecker-2.2-unix
-# $BENCHEXEC --numOfThreads $CPACHECKER_PARALLEL $MYBENCHDIR/cpachecker.xml
+cd $CPACHECKER_DIR
+$BENCHEXEC --numOfThreads $CPACHECKER_PARALLEL $MYBENCHDIR/cpachecker.xml
 
-# cd $MYBENCHDIR/../UAutomizer-linux
-# $BENCHEXEC --numOfThreads $ULTIMATE_PARALLEL $MYBENCHDIR/uautomizer.xml
+cd $DARTAGNAN_DIR
+$BENCHEXEC --numOfThreads $DARTAGNAN_PARALLEL $MYBENCHDIR/dartagnan.xml
 
-# Generate table with merged results and witness validation results
+cd $DEAGLE_DIR
+$BENCHEXEC --numOfThreads $DEAGLE_PARALLEL $MYBENCHDIR/deagle.xml
+
+cd $LOCKSMITH_DIR
+$BENCHEXEC --numOfThreads $LOCKSMITH_PARALLEL $MYBENCHDIR/locksmith.xml
+
+cd $THETA_DIR
+$BENCHEXEC --numOfThreads $THETA_PARALLEL $MYBENCHDIR/theta.xml
+
+cd $ULTIMATE_DIR
+$BENCHEXEC --numOfThreads $ULTIMATE_PARALLEL $MYBENCHDIR/uautomizer.xml
+
+# Generate table with merged results
 cd $RESULTSDIR
 cp $MYBENCHDIR/table-generator.xml table-generator.xml
 table-generator -x table-generator.xml
-# cp $MYBENCHDIR/table-generator-full.xml table-generator-full.xml
-# table-generator -x table-generator-full.xml -n full
 
 # Decompress all tool outputs for table HTML links
-unzip -o goblint.*.logfiles.zip
-# unzip -o cpachecker.*.logfiles.zip
-# unzip -o uautomizer.*.logfiles.zip
-# unzip -o witness2invariant-tmp.*.logfiles.zip
-# unzip -o goblint-guided-tmp.*.logfiles.zip
-# unzip -o goblint-validate-tmp.*.logfiles.zip
+# unzip -o goblint.*.logfiles.zip
+unzip -o cpachecker.*.logfiles.zip
+unzip -o dartagnan.*.logfiles.zip
+unzip -o deagle.*.logfiles.zip
+unzip -o locksmith.*.logfiles.zip
+unzip -o theta.*.logfiles.zip
+unzip -o uautomizer.*.logfiles.zip
