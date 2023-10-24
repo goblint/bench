03057da27d2f0fc3c293593a7093c308ced5bec5
Simmo Saan <simmo.saan@gmail.com>
2023-10-24 18:12:25 +0300

    Add racy versions of some race-challenges

---
+++ concrat/race-challenges/per-thread-array-index-race.c
@@ -0,0 +1,40 @@
+// Per-thread array index passed via argument.
+#include <stdlib.h>
+#include <pthread.h>
+extern void abort(void);
+void assume_abort_if_not(int cond) {
+  if(!cond) {abort();}
+}
+extern int __VERIFIER_nondet_int();
+
+int *datas;
+
+void *thread(void *arg) {
+  int i = arg;
+  i /= 2;
+  datas[i] = __VERIFIER_nondet_int(); // RACE!
+  return NULL;
+}
+
+int main() {
+  int threads_total = __VERIFIER_nondet_int();
+  assume_abort_if_not(threads_total >= 0);
+
+  pthread_t *tids = malloc(threads_total * sizeof(pthread_t));
+  datas = malloc(threads_total * sizeof(int));
+
+  // create threads
+  for (int i = 0; i < threads_total; i++) {
+    pthread_create(&tids[i], NULL, &thread, i); // may fail but doesn't matter
+  }
+
+  // join threads
+  for (int i = 0; i < threads_total; i++) {
+    pthread_join(tids[i], NULL);
+  }
+
+  free(tids);
+  free(datas);
+
+  return 0;
+}
