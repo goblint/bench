d4a83bc19e45b5b009c81fb497f83d53c18ecc4d
Simmo Saan <simmo.saan@gmail.com>
2023-10-31 10:19:35 +0000

    Run Concrat benchmarks with SV-COMP limits

--- concrat/run.sh
+++ concrat/run.sh
@@ -5,26 +5,28 @@ set -e
 
 MYBENCHDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # RESULTSDIR=$MYBENCHDIR/../results/eval-prec
-RESULTSDIR=/mnt/goblint-svcomp/benchexec/results/pldi-12-race-challenges
+RESULTSDIR=/mnt/goblint-svcomp/benchexec/results/pldi-19-concrat-svcomp-limits
 # GOBLINTDIR=$MYBENCHDIR/../goblint
 GOBLINTDIR=/mnt/goblint-svcomp/sv-comp/goblint
-GOBLINT_PARALLEL=7
+GOBLINT_PARALLEL=2
 GOBLINT23DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/goblint
-GOBLINT23_PARALLEL=7
+GOBLINT23_PARALLEL=2
 CPACHECKER_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/CPAchecker-2.2-unix
-CPACHECKER_PARALLEL=7
+CPACHECKER_PARALLEL=2
 DARTAGNAN_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/dartagnan
-DARTAGNAN_PARALLEL=7
+DARTAGNAN_PARALLEL=2
 DEAGLE_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/deagle
-DEAGLE_PARALLEL=7
+DEAGLE_PARALLEL=2
 DEAGLE_PURE_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/deagle-pure
-DEAGLE_PURE_PARALLEL=7
+DEAGLE_PURE_PARALLEL=2
 LOCKSMITH_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/locksmith
-LOCKSMITH_PARALLEL=7
+LOCKSMITH_PARALLEL=2
 THETA_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/theta
-THETA_PARALLEL=7
+THETA_PARALLEL=2
 ULTIMATE_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/UAutomizer-linux
-ULTIMATE_PARALLEL=7
+ULTIMATE_PARALLEL=2
+TSAN_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/sv-sanitizers
+TSAN_PARALLEL=2
 
 # read-only and overlay dirs for Value too large for defined data type workaround
 BENCHEXEC="benchexec --read-only-dir / --overlay-dir . --overlay-dir /home --outputpath $RESULTSDIR"
@@ -59,6 +61,9 @@ $BENCHEXEC --numOfThreads $THETA_PARALLEL $MYBENCHDIR/theta.xml
 cd $ULTIMATE_DIR
 $BENCHEXEC --numOfThreads $ULTIMATE_PARALLEL $MYBENCHDIR/uautomizer.xml
 
+cd $TSAN_DIR
+$BENCHEXEC --numOfThreads $TSAN_PARALLEL $MYBENCHDIR/tsan.xml
+
 # Generate table with merged results
 cd $RESULTSDIR
 cp $MYBENCHDIR/table-generator.xml table-generator.xml
@@ -74,3 +79,4 @@ unzip -o deagle.*.logfiles.zip
 unzip -o locksmith.*.logfiles.zip
 unzip -o theta.*.logfiles.zip
 unzip -o uautomizer.*.logfiles.zip
+unzip -o tsan.*.logfiles.zip
