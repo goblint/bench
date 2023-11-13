15420da7529ee71c174c7fe00bb0728c72d097f7
Simmo Saan <simmo.saan@gmail.com>
2023-11-13 10:50:34 +0000

    Add Relay

--- concrat/race-challenges/run.sh
+++ concrat/race-challenges/run.sh
@@ -5,7 +5,7 @@ set -e
 
 MYBENCHDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # RESULTSDIR=$MYBENCHDIR/../results/eval-prec
-RESULTSDIR=/mnt/goblint-svcomp/benchexec/results/pldi-21-race-challenges-binomial
+RESULTSDIR=/mnt/goblint-svcomp/benchexec/results/pldi-23-race-challenges-relay
 # GOBLINTDIR=$MYBENCHDIR/../goblint
 GOBLINTDIR=/mnt/goblint-svcomp/sv-comp/goblint
 GOBLINT_PARALLEL=2
@@ -25,6 +25,8 @@ THETA_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/theta
 THETA_PARALLEL=2
 ULTIMATE_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/UAutomizer-linux
 ULTIMATE_PARALLEL=2
+RELAY_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/relay-sv
+RELAY_PARALLEL=2
 TSAN_DIR=/mnt/goblint-svcomp/benchexec/tools-pldi/sv-sanitizers
 TSAN_PARALLEL=2
 
@@ -34,35 +36,38 @@ BENCHEXEC="benchexec --read-only-dir / --overlay-dir . --overlay-dir /home --out
 mkdir $RESULTSDIR || true
 
 # Run verification
-cd $GOBLINTDIR
-$BENCHEXEC --numOfThreads $GOBLINT_PARALLEL $MYBENCHDIR/goblint.xml
+# cd $GOBLINTDIR
+# $BENCHEXEC --numOfThreads $GOBLINT_PARALLEL $MYBENCHDIR/goblint.xml
 
-cd $GOBLINT23DIR
-$BENCHEXEC --numOfThreads $GOBLINT23_PARALLEL $MYBENCHDIR/goblint23.xml
+# cd $GOBLINT23DIR
+# $BENCHEXEC --numOfThreads $GOBLINT23_PARALLEL $MYBENCHDIR/goblint23.xml
 
-cd $CPACHECKER_DIR
-$BENCHEXEC --numOfThreads $CPACHECKER_PARALLEL $MYBENCHDIR/cpachecker.xml
+# cd $CPACHECKER_DIR
+# $BENCHEXEC --numOfThreads $CPACHECKER_PARALLEL $MYBENCHDIR/cpachecker.xml
 
-cd $DARTAGNAN_DIR
-$BENCHEXEC --numOfThreads $DARTAGNAN_PARALLEL $MYBENCHDIR/dartagnan.xml
+# cd $DARTAGNAN_DIR
+# $BENCHEXEC --numOfThreads $DARTAGNAN_PARALLEL $MYBENCHDIR/dartagnan.xml
 
-cd $DEAGLE_DIR
-$BENCHEXEC --numOfThreads $DEAGLE_PARALLEL $MYBENCHDIR/deagle.xml
+# cd $DEAGLE_DIR
+# $BENCHEXEC --numOfThreads $DEAGLE_PARALLEL $MYBENCHDIR/deagle.xml
 
 # cd $DEAGLE_PURE_DIR
 # $BENCHEXEC --numOfThreads $DEAGLE_PURE_PARALLEL $MYBENCHDIR/deagle-pure.xml
 
-cd $LOCKSMITH_DIR
-$BENCHEXEC --numOfThreads $LOCKSMITH_PARALLEL $MYBENCHDIR/locksmith.xml
+# cd $LOCKSMITH_DIR
+# $BENCHEXEC --numOfThreads $LOCKSMITH_PARALLEL $MYBENCHDIR/locksmith.xml
 
-cd $THETA_DIR
-$BENCHEXEC --numOfThreads $THETA_PARALLEL $MYBENCHDIR/theta.xml
+# cd $THETA_DIR
+# $BENCHEXEC --numOfThreads $THETA_PARALLEL $MYBENCHDIR/theta.xml
 
-cd $ULTIMATE_DIR
-$BENCHEXEC --numOfThreads $ULTIMATE_PARALLEL $MYBENCHDIR/uautomizer.xml
+# cd $ULTIMATE_DIR
+# $BENCHEXEC --numOfThreads $ULTIMATE_PARALLEL $MYBENCHDIR/uautomizer.xml
 
-cd $TSAN_DIR
-$BENCHEXEC --numOfThreads $TSAN_PARALLEL $MYBENCHDIR/tsan.xml
+cd $RELAY_DIR
+$BENCHEXEC --numOfThreads $RELAY_PARALLEL $MYBENCHDIR/relay.xml
+
+# cd $TSAN_DIR
+# $BENCHEXEC --numOfThreads $TSAN_PARALLEL $MYBENCHDIR/tsan.xml
 
 # Generate table with merged results
 cd $RESULTSDIR
@@ -70,13 +75,14 @@ cp $MYBENCHDIR/table-generator.xml table-generator.xml
 table-generator -x table-generator.xml
 
 # Decompress all tool outputs for table HTML links
-unzip -o goblint.*.logfiles.zip
-unzip -o goblint23.*.logfiles.zip
-unzip -o cpachecker.*.logfiles.zip
-unzip -o dartagnan.*.logfiles.zip
-unzip -o deagle.*.logfiles.zip
+# unzip -o goblint.*.logfiles.zip
+# unzip -o goblint23.*.logfiles.zip
+# unzip -o cpachecker.*.logfiles.zip
+# unzip -o dartagnan.*.logfiles.zip
+# unzip -o deagle.*.logfiles.zip
 # unzip -o deagle-pure.*.logfiles.zip
-unzip -o locksmith.*.logfiles.zip
-unzip -o theta.*.logfiles.zip
-unzip -o uautomizer.*.logfiles.zip
-unzip -o tsan.*.logfiles.zip
+# unzip -o locksmith.*.logfiles.zip
+# unzip -o theta.*.logfiles.zip
+# unzip -o uautomizer.*.logfiles.zip
+unzip -o relay.*.logfiles.zip
+# unzip -o tsan.*.logfiles.zip
