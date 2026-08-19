0ba286e78a0f13ede254def6542433dcf0e1c2f8
Simmo Saan <simmo.saan@gmail.com>
2026-08-19 13:56:25 +0000

    Add goblint-qsolver to be more like td3

--- goblint-qsolver/run.sh
+++ goblint-qsolver/run.sh
@@ -4,7 +4,7 @@ shopt -s extglob
 set -e
 
 BENCH_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-RESULTS_DIR=/mnt/goblint-svcomp/benchexec/results/qsolver-7-aug10-15min
+RESULTS_DIR=/mnt/goblint-svcomp/benchexec/results/qsolver-9-like-td3
 GOBLINT_DIR=/mnt/ecoop/goblint-qsolver
 # GOBLINT_PARALLEL=2
 GOBLINT_PARALLEL=5
--- goblint-qsolver/svcomp/run.sh
+++ goblint-qsolver/svcomp/run.sh
@@ -5,10 +5,10 @@ set -e
 
 BENCH_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SV_BENCHMARKS_DIR=/mnt/goblint-svcomp/benchexec/sv-benchmarks
-RESULTS_DIR=/mnt/goblint-svcomp/benchexec/results/qsolver-8-svcomp-all-level01-aug10
+RESULTS_DIR=/mnt/goblint-svcomp/benchexec/results/qsolver-10-svcomp-all-level01-like-td3
 GOBLINT_DIR=/mnt/ecoop/goblint-qsolver
 GOBLINT_PARALLEL=14
-GOBLINT_SOLVERS=(td3 slr3 slr3t qss_cwnw qsr qsri qso_cwnw qsr_dead qsr_nosides qsr_nosides_dead)
+GOBLINT_SOLVERS=(td3 slr3 qss_cwnw qss_cwnw_w qsr qsr_dead qsr_nosides qsr_nosides_dead qsr_nosides_dead_w qso_cwnw qso_cwnw_w)
 
 # read-only and overlay dirs for Value too large for defined data type workaround
 BENCHEXEC="benchexec --read-only-dir / --overlay-dir . --overlay-dir /home --outputpath $RESULTS_DIR --results-per-taskset"
@@ -20,12 +20,14 @@ cd $RESULTS_DIR
 for SOLVER in "${GOBLINT_SOLVERS[@]}"; do
     sed -e "s|SV_BENCHMARKS_DIR|$SV_BENCHMARKS_DIR|" -e "s|SOLVER|$SOLVER|" $BENCH_DIR/goblint.xml > goblint-$SOLVER.xml
 done
+sed -e "s|SV_BENCHMARKS_DIR|$SV_BENCHMARKS_DIR|" $BENCH_DIR/goblint-td3_o_cwnw.xml > goblint-td3_o_cwnw.xml
 
 # Run verification
 cd $GOBLINT_DIR
 for SOLVER in "${GOBLINT_SOLVERS[@]}"; do
     $BENCHEXEC --numOfThreads $GOBLINT_PARALLEL $RESULTS_DIR/goblint-$SOLVER.xml
 done
+$BENCHEXEC --numOfThreads $GOBLINT_PARALLEL $RESULTS_DIR/goblint-td3_o_cwnw.xml
 
 # Generate table
 cd $RESULTS_DIR
